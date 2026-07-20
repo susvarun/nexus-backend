9d9ca4f744e66c84d566c62fb9097c5d3d70faf5
Varun <susvarun010@gmail.com>
2026-07-21 01:27:04 +0530

    add strict cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 app.use(express.json());
 
